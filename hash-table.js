a07d19f5542883ed54e9c0ad4cdcd71e6ad7c301
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-03-22 22:08:38 +0545

    added hastable constructor and method

--- hash-table.js
+++ hash-table.js
@@ -12,18 +12,27 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count=0;
+    this.capacity=numBuckets;
+    this.data=[];
+    for(let i=0;i<numBuckets;i++){
+      this.data.push(null);
+    }
   }
 
   hash(key) {
     // Your code here
+    return parseInt(sha256(key).substring(0,8),16);
   }
 
   hashMod(key) {
     // Your code here
+    return this.hash(key)%this.capacity;
   }
 
   insertNoCollisions(key, value) {
     // Your code here
+    
   }
 
   insertWithHashCollisions(key, value) {
