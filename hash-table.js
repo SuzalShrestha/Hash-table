2c8d56a563f7c1e0fd4ac66553d823f1770c7940
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-03-23 20:17:33 +0545

    hash phase 3 completed

--- hash-table.js
+++ hash-table.js
@@ -1,4 +1,4 @@
-const sha256 = require('js-sha256');
+const sha256 = require("js-sha256");
 
 class KeyValuePair {
   constructor(key, value) {
@@ -9,41 +9,53 @@ class KeyValuePair {
 }
 
 class HashTable {
-
   constructor(numBuckets = 4) {
     // Your code here
-    this.count=0;
-    this.capacity=numBuckets;
-    this.data=[];
-    for(let i=0;i<numBuckets;i++){
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = [];
+    for (let i = 0; i < numBuckets; i++) {
       this.data.push(null);
     }
   }
 
   hash(key) {
     // Your code here
-    return parseInt(sha256(key).substring(0,8),16);
+    return parseInt(sha256(key).substring(0, 8), 16);
   }
 
   hashMod(key) {
     // Your code here
-    return this.hash(key)%this.capacity;
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
     // Your code here
-    
+    let index = this.hashMod(key);
+    if (this.data[index] === null) {
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+    } else {
+      throw new Error("hash collision or same key/value pair already exists!");
+    }
   }
 
   insertWithHashCollisions(key, value) {
     // Your code here
+    let index = this.hashMod(key);
+    if (this.data[index] === null) {
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+    } else {
+      let curr = this.data[index];
+      this.data[index] = new KeyValuePair(key, value);
+      this.data[index].next = curr;
+      this.count++;
+    }
   }
-
   insert(key, value) {
     // Your code here
+    
   }
-
 }
-
-
 module.exports = HashTable;
